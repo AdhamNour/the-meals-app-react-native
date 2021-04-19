2345d75528e61e5853b300f62bb7fc6ce926b453
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-19 04:52:43 +0200

    finished video 11 from module 6

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MealsNavigator from './navigations/MealsNavigator'
+
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator />
   );
 }
 
