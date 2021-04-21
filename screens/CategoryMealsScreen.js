2afe15ceb0dcf1d7facfa941f2376134fc20f92f
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-04-21 12:32:22 +0200

    finished video 19 from module 6

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import { View, Text,StyleSheet,Button } from 'react-native';
-import {CATEGORIES} from '../data/dummydata'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../data/dummydata";
+import Colors from "../Constants/Colors";
 
 const CategoryMealsScreen = (props) => {
-    const catId = props.navigation.getParam('categoryId');
-    const selectedCategory = CATEGORIES.find(c => c.id===catId);
-    return ( <View style = {styles.screen}>
-        <Text>
-            The CategoryMealsScreen
-        </Text>
-        <Text>
-            {selectedCategory.title}
-        </Text>
-    </View> );
-}
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((c) => c.id === catId);
+  return (
+    <View style={styles.screen}>
+      <Text>The CategoryMealsScreen</Text>
+      <Text>{selectedCategory.title}</Text>
+    </View>
+  );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catid = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((c) => c.id === catid);
+  return {
+    title: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Colors.primaryColor,
+    },
+    headerTintColor: "white",
+  };
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default CategoryMealsScreen;
