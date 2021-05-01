48f621d799604a456c0f1b0b51c207a1f5c3e23c
AdhamNour <adhamnourelwaffaa@gmail.com>
2021-05-01 17:08:28 -0400

    finished video 23 from module 6

---
+++ models/meal.js
@@ -0,0 +1,31 @@
+class Meal {
+  constructor(
+    id,
+    catgoriesIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactusFree
+  ) {
+    this.id = id;
+    this.catgoriesIds = catgoriesIds;
+    this.title = title;
+    this.affordability = affordability;
+    this.complexity = complexity;
+    this.imageUrl = imageUrl;
+    this.duration = duration;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactusFree = isLactusFree;
+  }
+}
