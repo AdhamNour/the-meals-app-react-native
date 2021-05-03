4a347076aa4b83470f2f3247d9371fe2bba1c97d
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-03 12:43:50 +0200

    finished video 29 from module 6

--- navigations/MealsNavigator.js
+++ navigations/MealsNavigator.js
@@ -1,9 +1,11 @@
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
 import Colors from "../Constants/Colors";
 
 const MealsNavigators = createStackNavigator(
@@ -23,8 +25,12 @@ const MealsNavigators = createStackNavigator(
       },
       headerTintColor: "white",
     },
-    
   }
 );
 
-export default createAppContainer(MealsNavigators);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigators,
+  Favorites: FavoritesScreen,
+});
+
+export default createAppContainer(MealsFavTabNavigator);
