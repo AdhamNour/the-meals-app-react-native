247f62adcac08c25c1bf935a1b6ffbd520d8c31c
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-07 21:13:28 +0200

    finished video 38

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,16 +1,44 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ScrollView,
+  Image,
+} from "react-native";
 import { MEALS } from "../data/dummydata";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 
 const MealDetailsScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
-    <View style={styles.screen}>
-      <Text>The MealDetailsScreen</Text>
-      <Button title="Go to Cat" onPress={() => props.navigation.popToTop()} />
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <Text>{selectedMeal.duration}m</Text>
+        <Text>{selectedMeal.complexity.toUpperCase()}</Text>
+        <Text>{selectedMeal.affordability.toUpperCase()}</Text>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      <View style={styles.array}>
+        {selectedMeal.ingredients.map((item, index) => (
+          <Text key={index}>
+            {index + 1}) {item}
+          </Text>
+        ))}
+      </View>
+      <Text style={styles.title}>Steps</Text>
+      <View style={styles.array}>
+        {selectedMeal.steps.map((item, index) => (
+          <Text key={index}>
+            {index + 1}) {item}
+          </Text>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -19,7 +47,7 @@ MealDetailsScreen.navigationOptions = (navigationData) => {
   const mealx = MEALS.find((meal) => meal.id === mealId);
   return {
     title: mealx.title,
-    headerRight: ()=> (
+    headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Favorite"
@@ -32,9 +60,20 @@ MealDetailsScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  title: {
+    fontSize: 25,
+    textAlign: "center",
+  },
+  array: {
     alignItems: "center",
   },
 });
